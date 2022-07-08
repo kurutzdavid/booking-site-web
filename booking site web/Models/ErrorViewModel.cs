379f7e35a7dd4f9a69dad30e63aa1f570e0510ea
kurutzdavid <kurutzdavid5@gmail.com>
2022-07-08 10:44:11 +0300

    Add project files.

---
+++ booking site web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace booking_site_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
